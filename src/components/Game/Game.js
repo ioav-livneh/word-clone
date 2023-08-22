addc1136c375cb492b09c2946ed10d4b18f9775d
Ioav Livneh <ioavlivneh@Ioavs-MBP.localdomain>
2023-08-22 13:26:46 -0600

    completed basic wordle game without stretch goals

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import GuessInput from "../GuessInput";
+import GuessResults from "../GuessResults";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Banner from "../Banner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +13,35 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  // running / won / lost
+  const [gameStatus, setGameStatus] = React.useState("running");
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    const nextGuesses = [...guesses, tentativeGuess];
+    setGuesses(nextGuesses);
+
+    if (tentativeGuess === answer) {
+      setGameStatus("won");
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus("lost");
+    }
+  }
+
+  return (
+    <>
+      <Banner
+        gameStatus={gameStatus}
+        numOfGuesses={guesses.length}
+        answer={answer}
+      />
+      <GuessResults guesses={guesses} answer={answer} />
+      <GuessInput
+        handleSubmitGuess={handleSubmitGuess}
+        gameStatus={gameStatus}
+      />
+    </>
+  );
 }
 
 export default Game;
